b88aea18c1453f20bdcceb1a476ff384f4efa999
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2023-10-06 15:48:44 -0300

    feat: create theme selector, local storage logic and styles

---
+++ src/app/components/ThemeSelector/index.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { Sun, Moon } from "phosphor-react";
+import styles from "./styles.module.scss";
+import { useEffect, useState } from "react";
+
+type ThemeTypes = "LIGHT_THEME" | "DARK_THEME";
+
+function getDefaultTheme() {
+  if (localStorage.getItem("@web-dev-portfolio:theme-state-1.0.0") !== null) {
+    return localStorage.getItem(
+      "@web-dev-portfolio:theme-state-1.0.0"
+    ) as ThemeTypes;
+  }
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    return "DARK_THEME";
+  }
+
+  return "LIGHT_THEME";
+}
+
+export function ThemeSelector() {
+  const [theme, setTheme] = useState<ThemeTypes>(getDefaultTheme);
+
+  function handleThemeChange() {
+    setTheme((prev) => {
+      const current = prev === "DARK_THEME" ? "LIGHT_THEME" : "DARK_THEME";
+      localStorage.setItem("@web-dev-portfolio:theme-state-1.0.0", current);
+      document.documentElement.setAttribute("data-theme", current);
+      return current;
+    });
+  }
+
+  function computedLabel() {
+    if (theme === "DARK_THEME") {
+      return styles.label, styles.active;
+    }
+    return styles.label;
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", getDefaultTheme());
+  }, []);
+
+  return (
+    <div className={styles.theme}>
+      <input
+        type="checkbox"
+        id="theme-switch"
+        onChange={handleThemeChange}
+        value={theme}
+      />
+      <label htmlFor="theme-switch" className={computedLabel()}>
+        <Moon size="1.5rem" color="#f1d952" weight="fill" alt="Dark theme" />
+        <Sun size="1.5rem" color="#f39c12" weight="fill" alt="Light theme" />
+      </label>
+    </div>
+  );
+}
