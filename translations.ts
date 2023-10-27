ae3ca9d9b2f2cae2a4716610f3bbbee6df81482d
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2023-10-27 12:04:41 -0300

    feat: change json translations to be synchronous using a plain ts file due to server components not supporting async file imports

---
+++ translations.ts
@@ -0,0 +1,38 @@
+export const dictionary = {
+  ["en-US"]: {
+    homepage: {
+      label: "Front-End Developer",
+      heading: "Code, Creativity and Collaboration",
+      message:
+        "I love creating quality-driven web applications, delivering seamless user experiences, while solving complex challenges.",
+      link: "Let's Chat!",
+    },
+    theme_selector: {
+      light: "Switch to light theme",
+      dark: "Switch to dark theme",
+    },
+    document: {
+      title: "Danilo Nogueira",
+      description:
+        "Danilo Nogueira is a web developer with focus on font-end and specialized on React and Vue frameworks",
+    },
+  },
+  ["pt-BR"]: {
+    homepage: {
+      label: "Desenvolvedor Front-End",
+      heading: "Código, Criatividade e Colaboração",
+      message:
+        "Adoro criar aplicações web com foco em qualidade, proporcionando uma excelente experiência de usuário e, ao mesmo tempo, resolvendo desafios complexos.",
+      link: "Fale comigo!",
+    },
+    theme_selector: {
+      light: "Trocar para tema claro",
+      dark: "Trocar para tema escuro",
+    },
+    document: {
+      title: "Danilo Nogueira",
+      description:
+        "Danilo Nogueira é um desenvolvedor web com foco em front-end e especializado nos frameworks React e Vue",
+    },
+  },
+};
