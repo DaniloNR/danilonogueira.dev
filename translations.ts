4be494c1bd0c8654e994653cb4c8c91d3553f308
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2024-02-01 16:30:59 -0300

    feat: add career translations

--- translations.ts
+++ translations.ts
@@ -6,6 +6,41 @@ export const dictionary = {
       message:
         "I love creating quality-driven web applications, delivering seamless user experiences, while solving complex challenges.",
       link: "Let's Chat!",
+      techs: "Techstack",
+      testimonials: {
+        felipe: {
+          position: "Front-end developer",
+          text: [
+            "I had the privilege of working side by side with Danilo for five years, and during that time, he stood out as a true inspiration to the entire team.",
+            "Danilo demonstrated deep knowledge in frontend development from the beginning of our joint journey. As a teammate, his technical skills and passion for programming always stood out. He excels at turning complex concepts into elegant and functional solutions, making our projects truly remarkable in terms of user experience.",
+            "In addition to his impeccable technical skills, Danilo is an exceptional colleague and has always been willing to collaborate and share knowledge with the rest of the team. He has shown a remarkable commitment to our professional growth and improvement.",
+            "One thing I have always admired in Danilo is his constant pursuit of learning and staying up to date with the latest trends and advancements in the field of frontend development. His dedication to staying updated and seeking new challenges is inspiring.",
+            "Throughout our five years of working together, I could see in Danilo an exemplary work ethic. He faces obstacles with determination and has an innate ability to find creative solutions to the problems we encounter in our day-to-day work.",
+            "With all these qualities, I sincerely recommend Danilo for any opportunity in the frontend development field. He is a valuable addition to any team, and I am confident that he will continue to impress and inspire with his skills and dedication.",
+          ],
+        },
+        jack: {
+          position: "Product Designer | UX/UI Design | Interaction Design",
+          text: [
+            "Working with Danilo was an incredible experience. He is a professional with remarkable technical competence, always delivering high-quality solutions. His ability to solve complex challenges is impressive; he is indeed an excellent professional and colleague!",
+          ],
+        },
+        julio: {
+          position: "Senior Back End Engineer",
+          text: [
+            "During the time we had the opportunity to work together, I could witness his relentless pursuit of code quality excellence and his constant interest in staying updated on new technologies. Danilo is proactive and willing to tackle challenges, even the most complex ones.",
+            "A notable example of his professionalism and dedication was his crucial role in improving the Git workflow in the company, which resulted in the efficient organization of our pipelines and releases. Furthermore, he led projects involving microfrontends and automated frontend testing, which added immense technical value to all team members.",
+            "In addition to his technical skills, Danilo is an extremely easy person to relate to, always willing to help and with a remarkable collaborative spirit. I am sure that any team will be honored to have his expertise and positive personality.",
+          ],
+        },
+      },
+      intro: {
+        label: "Intro",
+        title: "Hello! Im Danilo Nogueira",
+        quote: "Every great software begins with a even greater idea",
+        content:
+          "I have started my journey on web development even before finishing my CS college degree nearly 7 years ago. During this time I have worked on a government education company and on a LMS startup that was acquired by a unicorn-candidate startup. I have collaborated with many talented people creating software for both business and consumer use. Through the years I have become passionate about software engineering and being involved in the product's creation process, applying my tech expertise to give life to those products.",
+      },
     },
     theme_selector: {
       light: "Switch to light theme",
@@ -16,6 +51,80 @@ export const dictionary = {
       description:
         "Danilo Nogueira is a web developer with focus on font-end and specialized on React and Vue frameworks",
     },
+    testimonial: {
+      linkedin_tooltip: "Check it on LinkedIn",
+      expand_message: "...read more",
+      translation_message: "Translated from portuguese",
+    },
+    career: {
+      main: {
+        title: "How it all started",
+        description:
+          "I've grown up using the internet almost on a daily basis and was always curious to understand how things actually worked and all the technology behind it. I decided to go to CS college and quickly got hooked on learning algorithms and the magic behind how computers work. During this time, my first contact with the web development world was working on a project that used Moodle with JavaScript. After that first experience, I really fell in love with web development and wanted to delve deeper and learn more, so I got a part-time job working as a backend developer, officially kickstarting my journey into the web developer world. After that, I worked as a full-time front-end developer at a SaaS startup for 4 years and learned a lot about how to build reliable software and a great user experience. Eventually, the startup was acquired by Gupy, an HR tech unicorn-candidate startup. This marked a significant chapter, where I continued as a senior front-end developer for another year.",
+      },
+      timeline: {
+        title: "Career",
+        download: "Download Resume",
+        jobs: [
+          {
+            position: "Software Engineer III",
+            company: {
+              name: "Gupy",
+              url: "https://www.linkedin.com/company/gupy/",
+            },
+            location: "Brazil",
+            type: "Full time (remote)",
+            period: {
+              dateTime: "2022-07/2023-07",
+              label: "Jul 2022 - Jul 2023 · 1 yr 1 mos",
+            },
+            description: [
+              "Microfrontend environment",
+              "Test driven development",
+              "On call rotation",
+              "Feature flag releases",
+            ],
+          },
+          {
+            position: "Senior Developer II",
+            company: {
+              name: "Niduu",
+              url: "https://niduu.com/",
+            },
+            location: "Brazil",
+            type: "Full time (on-site)",
+            period: {
+              dateTime: "2018-01/2022-06",
+              label: "Jan 2018 - Jun 2022 · 4 yr 6 mos",
+            },
+            description: [
+              "Developed admin system from scratch",
+              "Presentations with customers about the system",
+              "Constant support and updates",
+              "Multiple reports, charts and tools developed",
+            ],
+          },
+          {
+            position: "Junior Back End Developer",
+            company: {
+              name: "UNA-SUS/UFMA",
+              url: "https://www.linkedin.com/company/unasusufma/",
+            },
+            location: "Brazil",
+            type: "Part-time (on-site)",
+            period: {
+              dateTime: "2015-08/2017-08",
+              label: "Aug 2015 - Aug 2017 · 2 yrs 1 mos",
+            },
+            description: [
+              "Maintenance and development of APIs in PHP",
+              "Support and resolution of tickets from multiple systems",
+              "Development of internal tools with AngularJS",
+            ],
+          },
+        ],
+      },
+    },
   },
   ["pt-BR"]: {
     homepage: {
@@ -24,6 +133,41 @@ export const dictionary = {
       message:
         "Adoro criar aplicações web com foco em qualidade, proporcionando uma excelente experiência de usuário e, ao mesmo tempo, resolvendo desafios complexos.",
       link: "Fale comigo!",
+      techs: "Tecnologias",
+      testimonials: {
+        felipe: {
+          position: "Desenvolvedor Front-End",
+          text: [
+            "Tive o privilégio de trabalhar lado a lado com o Danilo por cinco anos, e durante esse período, ele se destacou como uma verdadeira inspiração para toda a equipe.",
+            "Danilo demonstrou um profundo conhecimento em desenvolvimento frontend desde o início de nossa jornada conjunta. Como colega de equipe, suas habilidades técnicas e paixão pela programação sempre se destacaram. Ele é especialista em transformar conceitos complexos em soluções elegantes e funcionais, tornando nossos projetos verdadeiramente  notáveis em termos de experiência do usuário.",
+            "Além de suas habilidades técnicas impecáveis, Danilo é um colega de trabalho excepcional e sempre esteve disposto a colaborar e compartilhar conhecimentos com o restante da equipe. Ele demonstrou um compromisso notável com nosso crescimento e aprimoramento profissional.",
+            "Uma coisa que sempre admirei em Danilo é sua busca constante pelo aprendizado e por acompanhar as últimas tendências e avanços na área de desenvolvimento frontend. Sua dedicação em se manter atualizado e buscar novos desafios é inspiradora.",
+            "Ao longo de nossos cinco anos de trabalho conjunto, pude ver em Danilo uma ética de trabalho exemplar. Ele enfrenta obstáculos com determinação e possui uma habilidade inata para encontrar soluções criativas para os problemas que enfrentamos em nosso dia a dia.",
+            "Com todas essas qualidades, recomendo sinceramente o Danilo para qualquer oportunidade na área de desenvolvimento frontend. Ele é uma adição valiosa para qualquer equipe e tenho certeza de que continuará a impressionar e inspirar com suas habilidades e dedicação.",
+          ],
+        },
+        jack: {
+          position: "Product Designer | UX/UI Design | Interaction Design",
+          text: [
+            "Trabalhar com o Danilo foi uma experiência incrível. Ele é um profissional de competência técnica notável, sempre entregando soluções de alta qualidade. Sua habilidade em resolver desafios complexos é impressionante, ele de fato é um ótimo profissional e colega de trabalho!",
+          ],
+        },
+        julio: {
+          position: "Senior Back End Engineer",
+          text: [
+            "Durante o período em que tivemos a oportunidade de trabalhar juntos, pude testemunhar sua busca incessante pela excelência na qualidade do código e seu interesse constante em estar atualizado sobre as novas tecnologias. Danilo é proativo e disposto a enfrentar desafios, mesmo os mais complexos.",
+            "Um exemplo notável do profissionalismo e dedicação, foi seu papel fundamental no aperfeiçoamento do fluxo do Git na empresa, o que resultou na organização eficiente de nossas pipelines e releases. Além disso, conduziu projetos com microfrontends e testes automatizados para frontend que agregou um imenso valor técnico a todos os colegas de equipe.",
+            "Além de suas habilidades técnicas, Danilo é uma pessoa extremamente fácil de se relacionar, sempre disposto a ajudar e com um espírito colaborativo notável. Tenho certeza de que qualquer equipe ficará honrada em contar com sua expertise e personalidade positiva.",
+          ],
+        },
+      },
+      intro: {
+        label: "Introdução",
+        title: "Olá! Eu sou Danilo Nogueira",
+        quote: "Todo software grandioso se inicia com uma ideia ainda maior",
+        content:
+          "Iniciei minha jornada em desenvolvimento web antes mesmo de concluir meu curso de Ciência da Computação, há quase 7 anos. Durante esse tempo, trabalhei em uma empresa governamental de educação e em uma startup de LMS que foi adquirida por uma startup candidata a unicórnio. Colaborei com muitas pessoas talentosas na criação de software tanto para uso empresarial quanto para o consumidor. Ao longo dos anos, me tornei apaixonado por engenharia de software e por participar do processo de criação do produto, aplicando minha experiência técnica para dar vida a esses produtos.",
+      },
     },
     theme_selector: {
       light: "Trocar para tema claro",
@@ -33,6 +177,80 @@ export const dictionary = {
       title: "Danilo Nogueira",
       description:
         "Danilo Nogueira é um desenvolvedor web com foco em front-end e especializado nos frameworks React e Vue",
+    },
+    testimonial: {
+      linkedin_tooltip: "Ver no LinkedIn",
+      expand_message: "...leia mais",
+      translation_message: "Traduzido do português",
+    },
+    career: {
+      main: {
+        title: "Como tudo começou",
+        description:
+          "Eu cresci usando a internet quase que diariamente e sempre tive curiosidade em entender como as coisas de fato funcionavam e toda a tecnologia por trás disso. Decidi cursar Ciência da Computação e rapidamente me vi obcecado em aprender algoritmos e a magia por trás de como os computadores funcionam. Durante esse tempo, meu primeiro contato com o mundo do desenvolvimento web foi trabalhando em um projeto que utilizava o Moodle com JavaScript. Após essa primeira experiência, realmente me apaixonei pelo desenvolvimento web e quis aprofundar meus conhecimentos, então consegui um estágio como desenvolvedor de backend, oficialmente iniciando minha jornada no mundo do desenvolvimento web. Depois disso, trabalhei como desenvolvedor front-end em tempo integral em uma startup de SaaS por 4 anos e aprendi muito sobre como construir software confiável e proporcionar uma ótima experiência ao usuário. Eventualmente, a startup foi adquirida pela Gupy, uma startup unicórnio de tecnologia em RH. Isso marcou um capítulo significativo, onde continuei como desenvolvedor front-end sênior por mais um ano.",
+      },
+      timeline: {
+        title: "Carreira",
+        download: "Download do currículo",
+        jobs: [
+          {
+            position: "Software Engineer III",
+            company: {
+              name: "Gupy",
+              url: "https://www.linkedin.com/company/gupy/",
+            },
+            location: "Brasil",
+            type: "Tempo integral (remoto)",
+            period: {
+              dateTime: "2022-07/2023-07",
+              label: "Jul de 2022 - Jul de 2023 · 1 ano 1 mês",
+            },
+            description: [
+              "Ambiente em microfrontend",
+              "Desenvolvimento orientado a testes",
+              "Rotações on-call",
+              "Lançamento de funcionalidades com feature flags",
+            ],
+          },
+          {
+            position: "Programador Senior II",
+            company: {
+              name: "Niduu",
+              url: "https://niduu.com/",
+            },
+            location: "Brasil",
+            type: "Tempo integral (presencial)",
+            period: {
+              dateTime: "2018-01/2022-06",
+              label: "Jan de 2018 - Jun de 2022 · 4 anos 6 meses",
+            },
+            description: [
+              "Desenvolvimento do sistema Admin do zero",
+              "Apresentações com clientes sobre o sistema",
+              "Suporte e atualizações constantes",
+              "Múltiplos relatórios, gráficos e ferramentas desenvolvidas",
+            ],
+          },
+          {
+            position: "Desenvolvedor Back End Junior",
+            company: {
+              name: "UNA-SUS/UFMA",
+              url: "https://www.linkedin.com/company/unasusufma/",
+            },
+            location: "Brasil",
+            type: "Meio período (presencial)",
+            period: {
+              dateTime: "2015-08/2017-08",
+              label: "Ago de 2015 - Ago de 2017 · 2 anos 1 mês",
+            },
+            description: [
+              "Manutenção e desenvolvimento de APIs em PHP",
+              "Suporte e resolução de tickets de múltiplos sistemas",
+              "Desenvolvimento de ferramentas internas com AngularJS",
+            ],
+          },
+        ],
+      },
     },
   },
 };
